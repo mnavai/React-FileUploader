ad778d5fa6d700218b714a553d03db161c566ae7
mnavai <maryam.navaei@gmail.com>
2019-12-22 20:43:22 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+import SearchBar from './SearchBar/SearchBar';
+import FilterableList from './FilterableList/FilterableList';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      filterOption: 'All'
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <SearchBar
+          searchTerm={this.state.searchTerm}
+          filterOption={this.state.filterOptions}/>
+        <FilterableList
+          files={this.props.files}  
+          searchTerm={this.state.searchTerm}
+          filterOption={this.state.filterOptions}/>
+      </div>
+    );
+  }
+}
+
+export default App;
